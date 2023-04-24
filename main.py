58072611a256af1c4fa1017a0e99abb097c56a52
MarcusO17 <marcusonggeye@gmail.com>
2023-04-24 12:55:06 +0800

    args

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ from flask_restful import Api, Resource, reqparse
 app = Flask(__name__)
 api = Api(app)
 
-videos
-#Caching
+video_put_args = reqparse.RequestParser()
+#help (Error MSG), required
+video_put_args.add_argument("name",type=str, help="Name of the video is req", required = True)
+video_put_args.add_argument("views",type=int, help="Views of the video  is req", required = True)
+video_put_args.add_argument("likes",type=int, help="Likes of the video  is req", required = True)
+
+# temp Caching
 videos = {}
 
 class Video(Resource):
@@ -15,7 +20,9 @@ class Video(Resource):
     
     #Add Data
     def put(self, videoID):
-        return {}
+        args = video_put_args.parse_args()
+        videos[videoID] = args
+        return videos[videoID], 201
 
 api.add_resource(Video, "/video/<int:videoID>")
 
--- test.py
+++ test.py
@@ -2,6 +2,9 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-params = {"likes":10}
-response = requests.put(BASE+"video/1", params)
+params = {"name": "APIFLASK", "views": 10299, "likes":100}
+response = requests.put(BASE+"video/2", params)
+print(response.json())
+input() # Pause
+response = requests.get(BASE+"video/1", params)
 print(response.json())
